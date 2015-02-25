2c07542bd8edc334bf56e913bdb3f324c978d8bc
RageCageApp <ragecageapp@gmail.com>
2015-02-25 15:37:58 -0500

    fixed Router class

--- library/applicationbase/Router.php
+++ library/applicationbase/Router.php
@@ -12,19 +12,20 @@ class Router
         $action = isset($urlArray[0]) ? $urlArray[0] : '';
         array_shift($urlArray);
         //Third part of url is the query string
-        $queryString = isset($urlArray[0]) ? $urlArray[0] : '';
+        $queryString = $urlArray;
      
         //Ensure that controller/model name is in correct form (eg userController, userModel)
-        $moduleName = ucwords(trim($controller));
+        $moduleName = strtolower(trim($controller));
         $controllerName = $moduleName.'Controller';
         $modelName = $moduleName.'Model';
         
         //Create controller based on request
-        $dispatch = new Controller($controllerName,$action);
-
-        if ((int)method_exists($controller, $action)) {
+        $dispatch = new $controllerName($controllerName,$action);
+        if ((int)method_exists($controllerName, $action)) {
             call_user_func_array(array($dispatch,$action),$queryString);
         } else {
+            // Figure out better way to handle this error
+            echo 'ERROR: failed to find proper course of action for request';
             //********* ERROR: failed to find proper course of action for request *************** //
         }
     }
