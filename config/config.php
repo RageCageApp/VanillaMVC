0aa6209c04f5069ec4d62d7dae5f00876fa29cc2
RageCageApp <ragecageapp@gmail.com>
2015-02-25 10:43:08 -0500

    create config file to store config settings

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php   
+/************************************************************* 
+* Database related settings
+**************************************************************/
+
+$cfg['db']['hostname'] = 'localhost'; 
+$cfg['db']['username'] = 'root';
+$cfg['db']['password'] = '';
+$cfg['db']['database'] = 'pixafy_db';
+
+$cfg['global']['error_reporting'] = TRUE;
