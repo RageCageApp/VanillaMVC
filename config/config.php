c6b79bcab7e1d95e36dd89498edb74f2d85e61a1
RageCageApp <ragecageapp@gmail.com>
2015-02-25 14:46:02 -0500

    setup db constants

--- config/config.php
+++ config/config.php
@@ -8,4 +8,10 @@ $cfg['db']['username'] = 'root';
 $cfg['db']['password'] = '';
 $cfg['db']['database'] = 'pixafy_db';
 
+// define DB constant variables
+define('DB_HOST',       $cfg['db']['hostname']);
+define('DB_USER',       $cfg['db']['username']);
+define('DB_PASSWORD',   $cfg['db']['password']);
+define('DB_NAME',       $cfg['db']['database']);
+
 $cfg['global']['error_reporting'] = TRUE;
