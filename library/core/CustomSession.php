2158bff355b11dada07a0176cc88a71f02a44817
RageCageApp <ragecageapp@gmail.com>
2015-02-26 21:17:54 -0500

    Session class

--- library/core/CustomSession.php
+++ library/core/CustomSession.php
@@ -1,32 +1,39 @@
 <?php
+define('SESS_COOKIE_NAME', 'my_session');
+define('COOKIE_TABLE_NAME', 'my_sessions');
+
 class CustomSession
 {
 	protected $_application;
 
 	protected $_sessionID;
-	protected $_sessionData;
-
-	protected $_userData;
+	protected $_sessionUserData;
 
 	public function __construct()
 	{
 		$this->_application =&Controller::get_instance();
 		$this->_application->load_database();
 
-		$this->_setSessionID();
+		$this->_setSessionData();
 
 		$this->_loadSessionData();
 	}
 
-	protected function setSessionID()
+	// Sets data of session
+	protected function _setSessionData()
 	{
-		if(isset($_COOKIE['ci_session']))
-			$_sessionID = $_COOKIE['ci_session'];
+		if(isset($_COOKIE[SESS_COOKIE_NAME]))
+		{
+			$this->_sessionID = $_COOKIE[SESS_COOKIE_NAME];
+			$this->_loadSessionData();
+		}
+			
 		else
-			$this->createNewSession();
+			$this->_createNewSession();
 	}
 
-	protected function createNewSession()
+	// Creates new session
+	protected function _createNewSession()
 	{
 		$sessid = '';
 		while (strlen($sessid) < 32)
@@ -34,29 +41,40 @@ class CustomSession
 			$sessid .= mt_rand(0, mt_getrandmax());
 		}
 
-		$this->_userData = array(
-							'session_id'	=> md5(uniqid($sessid, TRUE)),
+		$this->_sessionUserData = array(
+							'session_id'    => md5(uniqid($sessid, TRUE)),
 							'user_data'		=> ''
 							);
 
-		$this->CI->db->query($this->CI->db->insert_string($this->sess_table_name, $this->userdata));
+		$this->_application->db->insert('my_sessions', $this->_sessionUserData);
 
-		// Write the cookie
-		$this->_set_cookie();
+		setcookie(
+			SESS_COOKIE_NAME,
+			$this->_sessionUserData['session_id'],
+			time()+10
+		);
 	}
 
-	protected function loadSessionData()
+	// Loads session data in userData class variable
+	protected function _loadSessionData()
 	{
-		$_COOKIE[$_sessionID];
+		//lookup session in DB
+		//unserialize the results
+		//store unserialized results in $this->_userData
 	}
 
-	protected function getSessionData()
+	// Returns sessionData variable
+	public function _getSessionData()
 	{
-		return $this->_sessionData;
+		return $this->_sessionUserData;
 	}
 
-	protected function setSessionData($data)
+	// Destroys session data
+	public function destroy()
 	{
-		$this->_sessionData = $data;
+		if(isset($_COOKIE[SESS_COOKIE_NAME]));
+			unset($_COOKIE[SESS_COOKIE_NAME]);
+
+		$this->_sessionUserData = NULL;
 	}
 }
