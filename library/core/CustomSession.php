783c5c22e14f110cb8fb9d4f1a89b1532ae72883
RageCageApp <ragecageapp@gmail.com>
2015-02-27 05:18:42 -0500

    load session data from db & get session data

--- library/core/CustomSession.php
+++ library/core/CustomSession.php
@@ -51,7 +51,7 @@ class CustomSession
 		setcookie(
 			SESS_COOKIE_NAME,
 			$this->_sessionUserData['session_id'],
-			time()+10
+			time()+3600
 		);
 	}
 
@@ -59,21 +59,52 @@ class CustomSession
 	protected function _loadSessionData()
 	{
 		//lookup session in DB
-		//unserialize the results
-		//store unserialized results in $this->_userData
+		$query = 
+		"SELECT 
+			MS.session_id, MS.user_data
+		FROM my_sessions MS 
+		WHERE 
+			MS.session_id = ?
+		LIMIT 1";
+
+		$result = $this->_application->db->query($query, array($this->_sessionID));	
+
+		$this->_sessionUserData = array(
+							'session_id'    => $this->_sessionID,
+							'user_data'		=> (count($result)) ? unserialize($result[0]['user_data']) : ''
+							);
 	}
 
 	// Returns sessionData variable
-	public function _getSessionData()
+	public function getSessionData()
 	{
 		return $this->_sessionUserData;
+	}
+
+	// Set sessionUserData
+	public function setSessionUserData($data)
+	{
+		if (count($data) > 0)
+		{
+			foreach ($data as $key => $val)
+			{
+				$this->_sessionUserData['user_data'][$key] = $val;
+			}
+
+			$this->_application->db->update('my_sessions', 
+											array('user_data' => serialize($this->_sessionUserData['user_data'])), 
+											array('session_id' => $this->_sessionID));
+		}
 	}
 
 	// Destroys session data
 	public function destroy()
 	{
-		if(isset($_COOKIE[SESS_COOKIE_NAME]));
-			unset($_COOKIE[SESS_COOKIE_NAME]);
+		setcookie(
+			SESS_COOKIE_NAME,
+			"",
+			time()-1
+		);
 
 		$this->_sessionUserData = NULL;
 	}
