ba3d7a566ee6798eb759cc040bcd6ba4edd8a231
RageCageApp <ragecageapp@gmail.com>
2015-03-01 18:38:29 -0500

    blank form can't be submitted jQuery

--- application/views/user/login_view.php
+++ application/views/user/login_view.php
@@ -6,13 +6,39 @@
 		<div style="float: left;">
 			<form method="post" action="/user/login">
 				Email:<br>
-				<input type="text" name="email">
+				<input type="text" name="email" id="email">
 				<br>
 				Password:<br>
-				<input type="password" name="password">
-				<input type="submit" value="Login">				
+				<input type="password" name="password" id="password">
+				<input type="submit" value="Login" id="submit">				
 			</form>	
 			<?php echo $error;?>	
 		</div>
 	</div>
 </div>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" ></script>
+<script type="text/javascript">
+$( document ).ready(function() {
+    $('#submit').click(function(event){
+
+    	var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    	
+    	if(!pattern.test($('#email').val())){
+    		alert("Enter a valid email please.");
+            event.preventDefault();
+    	}
+
+    	if($('#email').val().length == 0) {
+            alert("Email required");
+            event.preventDefault();
+        }
+
+    	if($('#password').val().length == 0) {
+            alert("Password required");
+            event.preventDefault();
+        }
+        
+    });
+});
+</script>
